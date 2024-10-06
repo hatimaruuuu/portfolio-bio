4c4890bb283c88711466618f1dbdc22c581be637
hatimaruuuu <hayata2504@gmail.com>
2024-10-06 23:07:06 +0900

    ENのRaulの追加

--- multiliugual-portfolio.tsx
+++ multiliugual-portfolio.tsx
@@ -1,7 +1,17 @@
 "use client"
 
-
 import { useState, useRef } from "react"
 import { motion, useScroll, useTransform } from "framer-motion"
 import { Github, X, Mail, Linkedin } from "lucide-react"
-
+const traslations = {
+    en: {
+        name: "Hayata Kobayashi"
+        role: "1st year undergraduate, Faculty of Economics, Hosei University"
+        "Oss Product Owner"
+        "Data engineer"
+        "Data scientist"
+        "Backend engineer"
+        "Machine learning engineer"
+        "Infrastructure engineer"
+    }
+}
