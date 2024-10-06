3169261f362d609629652b3300420d3ec77e5b98
hatimaruuuu <hayata2504@gmail.com>
2024-10-06 22:36:09 +0900

    import完成

--- multiliugual-portfolio.tsx
+++ multiliugual-portfolio.tsx
@@ -3,4 +3,5 @@
 
 import { useState, useRef } from "react"
 import { motion, useScroll, useTransform } from "framer-motion"
-import { Github, }
+import { Github, X, Mail, Linkedin } from "lucide-react"
+
