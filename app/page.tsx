37e914f2279a699f52729cb702381b5ed0ff140e
hatimaruuuu <hayata2504@gmail.com>
2024-10-13 16:00:56 +0900

    基盤を作成

---
+++ app/page.tsx
@@ -0,0 +1,4 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card"
+
